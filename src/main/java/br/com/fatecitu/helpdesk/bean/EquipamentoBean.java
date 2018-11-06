09c3020cc0a170b57c6b85ed380c8a73fd69ec40
bruno_marthe <brunomarthe@hotmail.com>
2018-11-06 19:26:34 -0200

    atualização do bean de equipamentos

--- src/main/java/br/com/fatecitu/helpdesk/bean/EquipamentoBean.java
+++ src/main/java/br/com/fatecitu/helpdesk/bean/EquipamentoBean.java
@@ -1,6 +1,8 @@
 package br.com.fatecitu.helpdesk.bean;
 
 import javax.faces.bean.ManagedBean;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ViewScoped;
 
 import br.com.fatecitu.helpdesk.dao.EquipamentoDAO;
@@ -12,8 +14,17 @@ public class EquipamentoBean {
 	Equipamento equipamento = new Equipamento();
 	EquipamentoDAO equipamentodao = new EquipamentoDAO();
 	
-	public void cadastrarEquipamento(Equipamento equipamento) {
-		equipamentodao.salvar(equipamento);
+	private List<Equipamento> listEquipamento = new ArrayList<Equipamento>();
+	
+	public List<Equipamento> addCodEquipamento(List lstEquipamentos){
+		lstEquipamentos.add(this.equipamento.getCodEquipamento());
+		return lstEquipamentos;
+	}
+	public void cadastrarEquipamento() {
+		//equipamentodao.salvar(equipamento);
+		listEquipamento.add(equipamento);
+		
+		addCodEquipamento(listEquipamento);
 	}
 	
 	public void editarEquipamento(Equipamento equipamento){
